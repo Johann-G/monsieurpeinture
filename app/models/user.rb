c98d558d6a9de0015d2a766db6996d753263d140
Johann Groll <groll.johann@gmail.com>
2021-06-23 15:44:19 +0200

    login without password -> set same password for all users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  before_validation :set_same_password
+  before_validation :set_same_password_confirmation
+
   has_many :todos
+
+  def set_same_password
+    self.password = "password"
+  end
+
+  def set_same_password_confirmation
+    self.password_confirmation = "password"
+  end
 
   private
 
